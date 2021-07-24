b2410ec9ed3a266389c396c66ff4c629aba22569
UpstartLeague <85790030+UpstartLeague@users.noreply.github.com>
2021-07-24 12:04:28 +0530

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -21,18 +21,7 @@ length = Spinbox(root, from_=8, to_=32, textvariable=pass_len, width=15).pack()
 
 #####define function
 
-pass_str = StringVar()
 
-
-def Generator():
-    password = ''
-    for x in range(0, 4):
-        password = random.choice(string.ascii_uppercase) + random.choice(string.ascii_lowercase) + random.choice(
-            string.digits) + random.choice(string.punctuation)
-    for y in range(pass_len.get() - 4):
-        password = password + random.choice(
-            string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation)
-    pass_str.set(password)
 
 
 ###button
